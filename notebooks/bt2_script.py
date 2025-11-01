b6a0797b454f20ff5d11373974b708b85c549e9d
JuanDiegoGallego <juandiego.gallenico@gmail.com>
2025-11-01 17:15:53 +0100

    Ransac on segments

--- notebooks/bt2_script.py
+++ notebooks/bt2_script.py
@@ -3,70 +3,66 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt 
 
-extractor = FeatureExtractor()
-
 # Cargamos la imagen
 image = np.load('./borders_0.npy')
 
-# Extraemos una línea usando RANSAC
-line = extractor.ransac_line_fitting(
-    edge_image=image.astype(bool), 
-    max_iterations=1000, 
-    distance_threshold=2.0, 
-    min_inliers=30, 
-    max_lsq_iterations=10,
-    erase=True
-)
+# Instanciamos el extractor de características con la imagen de bordes (binaria)
+feature_extractor = FeatureExtractor(edge_image=image.astype(np.bool))
 
-# Visualizamos la línea sobre la imagen original (solo los puntos que quedan dentro de la imagen)
-a, b, c = line
-height, width = image.shape
+# Almacenamos los segmentos detectados
+lines = []
+segments = []
 
-if (b < 1e-6 and b > -1e-6):
-    # Hay linea vertical
-    y_vals = np.array([0, height - 1])
-    x_vals = np.array([-c / a, -c / a])
-elif (a < 1e-6 and a > -1e-6):
-    # Hay linea horizontal
-    x_vals = np.array([0, width - 1])
-    y_vals = np.array([-c / b, -c / b])
-else:
-    # Calculamos dos puntos en los extremos de la imagen
-    x_vals = np.array([0, width - 1])
-    y_vals = (-a * x_vals - c) / b
-    if (y_vals[0] < 0):
-        y_vals[0] = 0
-        x_vals[0] = (-b * y_vals[0] - c) / a
-    elif (y_vals[0] >= height):
-        y_vals[0] = height - 1
-        x_vals[0] = (-b * y_vals[0] - c) / a
-    if (y_vals[1] < 0):
-        y_vals[1] = 0
-        x_vals[1] = (-b * y_vals[1] - c) / a
-    elif (y_vals[1] >= height):
-        y_vals[1] = height - 1
-        x_vals[1] = (-b * y_vals[1] - c) / a
+# Parámetros de iteración
+min_inliers = 100
+max_iterations = 500
+min_segment_length = 50.0
+iterations = 0
+
+# Iteramos para encontrar más líneas
+while iterations < max_iterations:
+    line, segment = feature_extractor.ransac_segment_fitting(
+        max_iterations=200,
+        distance_threshold=0.7,
+        density_threshold=0.2,
+        min_inliers=min_inliers,
+        max_lsq_iterations=0,
+        min_segment_length=min_segment_length,
+        erase=True
+    )
+    if (line is not None):
+        lines.append(line)
+    if (segment is not None):
+        segments.append(segment)
+    iterations += 1
+    if (iterations % 10) == 0:
+        min_inliers = min_inliers - 1  # Reducimos el umbral de inliers para encontrar líneas más débiles
+        min_segment_length = max(10.0, min_segment_length - 5.0)  # Reducimos la longitud mínima del segmento
+
+print(len(lines), "lines detected.")
+print(len(segments), "segments detected.")
+image = (image * 255).astype(np.uint8)
+image = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
+
+# for line in lines:
+#     a, b, c = line
+#     if b != 0:
+#         x0, y0 = 0, int(-c / b)
+#         x1, y1 = image.shape[1], int((-a * image.shape[1] - c) / b)
+#     else:
+#         x0, y0 = int(-c / a), 0
+#         x1, y1 = int(-c / a), image.shape[0]
+#     cv.line(image, (x0, y0), (x1, y1), (255, 0, 0), 1)
+
+for s in segments:
+    (x_start, y_start), (x_end, y_end) = s
+    cv.line(image, (int(x_start), int(y_start)), (int(x_end), int(y_end)), (0, 255, 0), 2)
+
 
 # Plotteamos la imagen y la línea
+plt.figure(figsize=(10, 10))
 plt.imshow(image, cmap='gray')
-plt.plot(x_vals, y_vals, color='red')
-plt.xlim(0, width - 1)
-plt.ylim(height - 1, 0)
-plt.title('RANSAC Line Fitting')
-plt.show()
-
-# Mostramos la imagen resultante tras borrar los inliers
-
-edge_image = extractor.remove_line(
-    edge_image=image.astype(bool),
-    line=line,
-    distance_threshold=2.0
-)
-edge_image = edge_image.astype(np.uint8) * 255
-
-plt.imshow(edge_image, cmap='gray')
-plt.title('Edge Image after Inlier Removal')
-plt.xlim(0, width - 1)
-plt.ylim(height - 1, 0)
+plt.title('Detected Lines using RANSAC')
+plt.axis('off')
 plt.show()
 
--- src/dgst/utils/features.py
+++ src/dgst/utils/features.py
@@ -83,13 +83,12 @@ class FeatureExtractor:
             raise ValueError("Distance threshold must be positive.")
 
         a, b, c = line
-        height, width = self._edge_image.shape
         yy, xx = np.nonzero(self._edge_image)
         distances = np.abs(a * xx + b * yy + c) / np.sqrt(a ** 2 + b ** 2)
         inlier_mask = distances <= distance_threshold
         self._edge_image[yy[inlier_mask], xx[inlier_mask]] = False
 
-    def windowed_ramsac_line_fitting(
+    def windowed_ransac_line_fitting(
             self,
             window_size: int,
             step: int,
@@ -149,3 +148,181 @@ class FeatureExtractor:
                         self._remove_line(adjusted_line, distance_threshold)
 
         return detected_lines, self._edge_image
+    
+    def get_line_support(
+            self,
+            line: tuple,
+            distance_threshold: float,
+            density_threshold: float,
+            min_segment_length: float = 0.0,
+            erase: bool = False
+    ) -> np.ndarray:
+        """Get the support points of a given line from the edge image. Takes only the pixels
+        that satisfy the line equation. Must be called after fitting a line with RANSAC
+        with erase=False.
+
+        Args:
+            line: A tuple (a, b, c) representing the line equation ax + by + c = 0
+            distance_threshold: Maximum distance from the line to consider a point as support.
+            density_threshold: Proportion of points that must satisfy the line equation to be considered a valid segment.
+            min_segment_length: Minimum length of the segment to be considered valid. Set to 0 to skip this check.
+            erase: If True, remove the support points from the edge image.
+        Returns:
+            (x_coords, y_coords): Arrays of x and y coordinates of the support points. None if not enough support.
+        """
+        if line is None or len(line) != 3:
+            raise ValueError("Line must be a tuple of (a, b, c).")
+        a, b, c = line
+        if abs(a) < 1e-6 and abs(b) < 1e-6:
+            raise ValueError("Invalid line parameters: a and b cannot both be zero.")
+        if density_threshold <= 0 or density_threshold > 1:
+            raise ValueError("Density threshold must be in the range (0, 1].")
+        if distance_threshold <= 0:
+            raise ValueError("Distance threshold must be positive.")
+
+        # Normalize line parameters
+        norm = np.sqrt(a ** 2 + b ** 2)
+        a_norm = a / norm
+        b_norm = b / norm
+        c_norm = c / norm
+
+        # Mask the points that satisfy the line equation
+        yy, xx = np.nonzero(self._edge_image)
+        distances = a_norm * xx + b_norm * yy + c_norm
+        inlier_mask = np.abs(distances) <= distance_threshold
+        
+        xx = xx[inlier_mask]
+        yy = yy[inlier_mask]
+        distances = distances[inlier_mask]
+        x_proj = xx - distances * a_norm
+        y_proj = yy - distances * b_norm
+        projections = np.array(list(zip(x_proj, y_proj)))
+
+        if len(xx) < 2:
+            return None # No support points
+
+        # Sort points, projections and the corresponding coordinate arrays with the same permutation
+        sort_col = 0 if abs(b_norm) > abs(a_norm) else 1
+        sorted_idx = np.argsort(projections[:, sort_col])
+        projections = projections[sorted_idx]
+        xx = xx[sorted_idx]
+        yy = yy[sorted_idx]
+        # Store distance to first point
+        points = np.linalg.norm(projections - projections[0], axis=1)
+        N = len(points)
+
+        ## Kadane algorithm to find longest segment with points within distance threshold
+        
+        # Initialize
+        A = [] # A[k] = 1 - thr * (points[k+1]-points[k])
+        for i in range(N - 1):
+            delta_x = points[i+1]-points[i]
+            A.append(1 - density_threshold * delta_x)
+        global_max_sum = 0.0
+        current_max_sum = 0.0
+        best_start_index = -1
+        best_end_index = -1
+        temp_start_index = 0
+
+        # Loop over array A
+        for k in range(len(A)):
+            # Expand the current segment 
+            current_max_sum = current_max_sum + A[k]
+            
+            # If the current segment is better than the best found so far, update best segment
+            if current_max_sum > global_max_sum:
+                global_max_sum = current_max_sum
+                best_start_index = temp_start_index # Current segment starts at 'temp'
+                best_end_index = k + 1              # And ends at k+1 (since A has length N-1)
+            
+            # If the current sum is non-positive, reset it and move the start index
+            if current_max_sum <= 0:
+                current_max_sum = 0.0
+                temp_start_index = k + 1
+
+        # Compute the metrics of the found segment
+        if global_max_sum > 0:
+            # The indices are from the original array of POINTS
+            start_x = xx[best_start_index]
+            start_y = yy[best_start_index]
+            end_x = xx[best_end_index]
+            end_y = yy[best_end_index]
+            ans = ([start_x, start_y], [end_x, end_y])
+
+            if erase:
+                # Remove support points from edge image
+                for i in range(best_start_index, best_end_index + 1):
+                    self._edge_image[yy[i], xx[i]] = False
+
+            if min_segment_length > 0.0:
+                length = np.sqrt((end_x - start_x) ** 2 + (end_y - start_y) ** 2)
+                if length < min_segment_length:
+                    return None
+                
+            return ans
+        else:
+            return None
+        
+    def ransac_segment_fitting(
+            self,
+            max_iterations: int,
+            distance_threshold: float,
+            density_threshold: float,
+            min_inliers: int,
+            max_lsq_iterations: int = 0,
+            min_segment_length: float = 0.0,
+            erase: bool = False
+    ) -> tuple:
+        """Fit a line to edge points using RANSAC and get its support segment.
+
+        Args:
+            max_iterations: Number of RANSAC iterations
+            distance_threshold: Distance threshold to consider a point as an inlier
+            density_threshold: Proportion of points that must satisfy the line equation to be considered a valid segment.
+            min_inliers: Minimum number of inliers to accept a model
+            max_lsq_iterations: Number of least squares refinement iterations. Set to 0 to skip refinement.
+            min_segment_length: Minimum length of the segment to be considered valid. Set to 0 to skip this check.
+            erase: If True two case are possible:
+                If a no support segment is found, remove the inliers of the detected line from the edge image.
+                If a support segment is found, remove the support points from the edge image.
+        
+        Returns:
+           One segment endpoints as ([x_start, x_end], [y_start, y_end]) or None if no valid segment found.
+
+        """
+        # First, fit a line using RANSAC
+        line, _ = self.ransac_line_fitting(
+            max_iterations=max_iterations,
+            distance_threshold=distance_threshold,
+            min_inliers=min_inliers,
+            max_lsq_iterations=max_lsq_iterations,
+            erase=False
+        )
+
+        # If no line found, return None
+        if line is None:
+            return None, None
+
+        # Then, get the support segment of the fitted line
+        segment = self.get_line_support(
+            line=line,
+            distance_threshold=distance_threshold,
+            density_threshold=density_threshold,
+            min_segment_length=min_segment_length,
+            erase=erase # Support points will be erased even if segment length is smaller than min_segment_length
+        )
+
+        if segment is None and erase:
+            # Remove inliers of the detected line from the edge image
+            self._remove_line(line, distance_threshold)
+
+        return line, segment
+    
+    @property
+    def image(self) -> np.ndarray:
+        """Get the current edge image.
+
+        Returns:
+            The current edge image.
+        """
+        return self._edge_image
